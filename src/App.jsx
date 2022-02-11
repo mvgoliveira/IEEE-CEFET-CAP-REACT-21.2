b64cd62f4d34aa301df5aa97e8cbc5bc68f2b95d
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-11 01:32:18 -0300

    parte 9 - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,116 @@
+import { useState } from "react";
+import GlobalStyles from "./globalStyles"
+import { Calculator } from "./styles"
 
 function App() {
+  const [n1, setN1] = useState("")
+  const [n2, setN2] = useState("")
+  const [operator, setOperator] = useState("")
+  const [res, setRes] = useState("")
+
+  function addNumber(number) {
+    if (res !== "" && operator === "") {
+      erase();
+    }
+
+    if (operator === "") {
+      document.getElementById("display").innerHTML += number;
+      setN1(n1 + number);
+    } else if (operator !== "" && n2 === "") {
+      document.getElementById("display").innerHTML = "";
+      document.getElementById("display").innerHTML = number;
+      setN2(number);
+    } else {
+      document.getElementById("display").innerHTML += number;
+      setN2(n2 + number);
+    }
+  }
+
+  function erase() {
+    setN1("");
+    setN2("");
+    setOperator("");
+    setRes("");
+
+    document.getElementById("display").innerHTML = "";
+  }
+
+  function addOperator(operatorSelected) {
+    if (n1 !== "") {
+      setOperator(operatorSelected)
+    }
+  }
+
+  function calculate(event) {
+    event.preventDefault();
+
+    if (n1 !== "" && n2 !== "" && operator !== "") {
+      switch (operator) {
+        case "+":
+          setRes(parseInt(n1) + parseInt(n2));
+          break;
+          
+          case "-":
+          setRes(parseInt(n1) - parseInt(n2));
+          break;
+          
+          case "*":
+          setRes(parseInt(n1) * parseInt(n2));
+          break;
+          
+          case "/":
+          setRes(parseInt(n1) / parseInt(n2));
+          break;
+      
+        default:
+          break;
+      }   
+
+      document.getElementById("display").innerHTML = "" + res.toFixed(2);
+
+      setN1("" + res);
+      setN2("");
+      setOperator("");
+    }
+  }
+
   return (
-    <h1>Hello World</h1>
+    <Calculator>
+      <GlobalStyles/>
+      <div id="calculator">
+        <form action="" onSubmit={calculate}>
+          <span id="display"></span>
+
+          <section id="buttonsGrid">
+            <article id="numbersGrid">
+              <button type="button" className="operatorButton" onClick={() => erase()}> C </button>
+              <button type="button" className="operatorButtonOff">  </button>
+              <button type="button" className="operatorButtonOff">  </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('7')}> 7 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('8')}> 8 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('9')}> 9 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('4')}> 4 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('5')}> 5 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('6')}> 6 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('1')}> 1 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('2')}> 2 </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('3')}> 3 </button>
+              <button type="button" className="numbersButtonOff">  </button>
+              <button type="button" className="numbersButton" onClick={() => addNumber('0')}> 0 </button>
+              <button type="button" className="numbersButtonOff">  </button>
+            </article>
+            
+            <article id="operatorsGrid">
+              <button type="button" className="operatorButton" onClick={() => addOperator('/')}> ÷ </button>
+              <button type="button" className="operatorButton" onClick={() => addOperator('*')}> x </button>
+              <button type="button" className="operatorButton" onClick={() => addOperator('-')}> - </button>
+              <button type="button" className="operatorButton" onClick={() => addOperator('+')}> + </button>
+              <button type="submit" className="submitButton"> = </button>
+            </article>
+          </section>
+        </form>
+      </div>
+    </Calculator>
   )
 }
 
