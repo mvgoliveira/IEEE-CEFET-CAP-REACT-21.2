7b5287e5e3264f6884a94d6b186216fff011c7a1
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-11 00:34:47 -0300

    Parte 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Home} from './pages/Home';
 import {About} from './pages/About';
+import GlobalStyle from './styles/globalStyle';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="about" element={<About />}/>
       </Routes>
+      <GlobalStyle/>
     </BrowserRouter>
+    </>
   );
 }
 
