8629c7ff6cc7cec78ca7797d687e94e4ecafcb10
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-11 02:07:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GlobalStyles from "./globalStyles"
 import { Calculator } from "./styles"
 
@@ -65,51 +65,52 @@ function App() {
         default:
           break;
       }   
-
+    }
+  }
+  
+  useEffect(() => {
+    if (res !== "") {
       document.getElementById("display").innerHTML = "" + res.toFixed(2);
-
       setN1("" + res);
       setN2("");
       setOperator("");
     }
-  }
+  }, [res]);
 
   return (
     <Calculator>
       <GlobalStyles/>
-      <div id="calculator">
-        <form action="" onSubmit={calculate}>
-          <span id="display"></span>
+      <form action="" onSubmit={calculate}>
+        <span id="display"></span>
 
-          <section id="buttonsGrid">
-            <article id="numbersGrid">
-              <button type="button" className="operatorButton" onClick={() => erase()}> C </button>
-              <button type="button" className="operatorButtonOff">  </button>
-              <button type="button" className="operatorButtonOff">  </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('7')}> 7 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('8')}> 8 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('9')}> 9 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('4')}> 4 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('5')}> 5 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('6')}> 6 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('1')}> 1 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('2')}> 2 </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('3')}> 3 </button>
-              <button type="button" className="numbersButtonOff">  </button>
-              <button type="button" className="numbersButton" onClick={() => addNumber('0')}> 0 </button>
-              <button type="button" className="numbersButtonOff">  </button>
-            </article>
-            
-            <article id="operatorsGrid">
-              <button type="button" className="operatorButton" onClick={() => addOperator('/')}> ÷ </button>
-              <button type="button" className="operatorButton" onClick={() => addOperator('*')}> x </button>
-              <button type="button" className="operatorButton" onClick={() => addOperator('-')}> - </button>
-              <button type="button" className="operatorButton" onClick={() => addOperator('+')}> + </button>
-              <button type="submit" className="submitButton"> = </button>
-            </article>
-          </section>
-        </form>
-      </div>
+        <section id="buttonsGrid">
+          <article id="numbersGrid">
+            <button type="button" className="operatorButton" onClick={() => erase()}> C </button>
+            <button type="button" className="operatorButtonOff">  </button>
+            <button type="button" className="operatorButtonOff">  </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('7')}> 7 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('8')}> 8 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('9')}> 9 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('4')}> 4 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('5')}> 5 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('6')}> 6 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('1')}> 1 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('2')}> 2 </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('3')}> 3 </button>
+            <button type="button" className="numbersButtonOff">  </button>
+            <button type="button" className="numbersButton" onClick={() => addNumber('0')}> 0 </button>
+            <button type="button" className="numbersButtonOff">  </button>
+          </article>
+          
+          <article id="operatorsGrid">
+            <button type="button" className="operatorButton" onClick={() => addOperator('/')}> ÷ </button>
+            <button type="button" className="operatorButton" onClick={() => addOperator('*')}> x </button>
+            <button type="button" className="operatorButton" onClick={() => addOperator('-')}> - </button>
+            <button type="button" className="operatorButton" onClick={() => addOperator('+')}> + </button>
+            <button type="submit" className="submitButton"> = </button>
+          </article>
+        </section>
+      </form>
     </Calculator>
   )
 }
