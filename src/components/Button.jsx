ec1fcf0a4c407a510b97c2e100cff90d11db24b9
João Henrique Terêncio <94974380+Jh-Terencio@users.noreply.github.com>
2022-12-08 12:11:03 -0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,7 @@
+//Agora nos adicionamos uma variavel fx, mas para que ela serve?
 export default function Button({text, fx, children}) {
   return (
+    //O fx será uma variavel que sempre que o botão for clicado ela será executada, por isso usamos a função onClick que executa tudo isso
     <button type="button" onClick={fx}>
       {text}
       {children}
